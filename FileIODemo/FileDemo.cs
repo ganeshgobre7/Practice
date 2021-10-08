a4d52e734980871a0524a40e1f549411a83c1a32
ganeshgobre7 <ganeshgobre7@gmail.com>
2021-10-08 19:22:47 +0530

    Check file exist or not

---
+++ FileIODemo/FileDemo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace FileIODemo
+{
+    class FileDemo
+    {
+        public void CheckFile()
+        {
+            string path = @"D:\B.LAB\C sharp\FileIODemo\FileIODemo\FileDemo1.txt";
+            File.Exists(path);
+        }
+    }
+}
---
+++ FileIODemo/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace FileIODemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("WelCome to FileIO");
+            FileDemo file = new FileDemo();
+            file.CheckFile();
+
+        }
+    }
+}
